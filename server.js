232185f2190dfa0a9f0f03863564f8934d230cd3
SamuelColmanBjj <samucol2004@gmail.com>
2025-11-20 17:55:37 -0500

    s

--- server.js
+++
@@ -1,5 +0,0 @@
-// server.js
-var app = require('./index'); // o './server' según tu nombre de archivo
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
